e3a801485acedfae01d26f172b6197e1e68e3545
alcien <alcien@nate.com>
2024-11-19 16:25:42 +0900

    Add files via upload

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 from huggingface_hub import hf_hub_download
 import os
 import shutil
+import subprocess
+
 
 fd1 = 't1207_av'
 fd2 = 'train1207'
@@ -20,9 +22,21 @@ print(f"Downloaded file path: {file_path}")
 file_path2 = hf_hub_download(repo_id="kaya218/test", filename= "video.ckpt")
 print(f"Downloaded file path: {file_path2}")
 
+file_path3 = hf_hub_download(repo_id="kaya218/test", filename= "last_video.ckpt")
+print(f"Downloaded file path: {file_path3}")
+
+url = 'https://huggingface.co/kaya218/test/resolve/main/last_av.ckpt?download=true'
+
+output_path = os.path.join(cpath, fd1, 'last.ckpt')
+
+
 
 shutil.copy(file_path, os.path.join(cpath,fd1,'av.ckpt'))
 shutil.copy(file_path2, os.path.join(cpath, fd2, 'video.ckpt'))
 
-print(f'moved into {fd1} and {fd2} folders')
+shutil.copy(file_path3, os.path.join(cpath, fd2, 'last.ckpt'))
+
+subprocess.run(['wget', '-O', output_path, url], check=True)
+
+print(f'moved downloaded files into {fd1} and {fd2} folders')
 
